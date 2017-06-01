1c673d6a668e921779cf174e7f0f90c41e7326b1
Vlaeh <Vlaeh@users.noreply.github.com>
2017-06-01 17:06:59 +0200

    Importing version 1.0 of the mod to github

---
+++ src/main/java/vlaeh/minecraft/forge/locate/Locate.java
@@ -0,0 +1,36 @@
+package vlaeh.minecraft.forge.locate;
+
+import net.minecraftforge.common.config.Configuration;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
+
+@Mod(modid = Locate.MODID, //
+        version = Locate.VERSION, //
+        name = Locate.NAME, //
+        acceptableRemoteVersions = "*", //
+        acceptedMinecraftVersions = "[1.10,1.12)" //
+)
+public class Locate {
+    public static final String MODID = "locate";
+    public static final String VERSION = "1.0";
+    public static final String NAME = "Locate for 1.10";
+
+    public static Configuration config;
+    public static boolean skipEnabled = true;
+    public static boolean messageEnabled = true;
+    public static boolean statusEnabled = false;
+    public static boolean clearWeather = true;
+    public static boolean setSpawnDuringDayEnabled = false;
+    public static int ratio = 50;
+
+    @Mod.Instance
+    public static Locate instance;
+
+    @EventHandler
+    public void serverLoad(FMLServerStartingEvent event) {
+        System.out.println("Registering command /" + LocateCommand.COMMAND);
+        event.registerServerCommand(new LocateCommand());
+    }
+
+}
---
+++ src/main/java/vlaeh/minecraft/forge/locate/LocateCommand.java
@@ -0,0 +1,138 @@
+package vlaeh.minecraft.forge.locate;
+
+import java.lang.reflect.Field;
+import java.util.Collections;
+import java.util.List;
+
+import org.apache.commons.lang3.reflect.FieldUtils;
+
+import net.minecraft.command.CommandBase;
+import net.minecraft.command.CommandException;
+import net.minecraft.command.ICommandSender;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.Style;
+import net.minecraft.util.text.TextComponentString;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.world.World;
+import net.minecraft.world.WorldServer;
+import net.minecraft.world.chunk.IChunkGenerator;
+import net.minecraft.world.gen.ChunkProviderServer;
+import net.minecraft.world.gen.structure.MapGenStructure;
+
+public class LocateCommand extends CommandBase {
+    public static final String COMMAND = "locate";
+    public static final String USAGE = "/" + COMMAND + " Stronghold Monument Village EndCity Fortress Temple Mineshaft";
+
+    @Override
+    public String getName() {
+        return COMMAND;
+    }
+
+    @Override
+    public String getUsage(ICommandSender sender) {
+        return USAGE;
+    }
+
+    public void usage(final ICommandSender sender) {
+        sender.sendMessage(
+                new TextComponentString("Usage: " + USAGE).setStyle(new Style().setColor(TextFormatting.RED)));
+    }
+
+    @Override
+    public void execute(final MinecraftServer server, final ICommandSender sender, final String[] args)
+            throws CommandException {
+        if (args.length != 1) {
+            usage(sender);
+            return;
+        }
+        final String structure = args[0];
+
+        final World entityWorld = sender.getEntityWorld();
+        if (!(entityWorld instanceof WorldServer)) {
+            sender.sendMessage(new TextComponentString("Invalid server type " + entityWorld.getClass()));
+        }
+        final BlockPos position = sender.getPosition();
+        final WorldServer serverWorld = (WorldServer) entityWorld;
+        final ChunkProviderServer chunkProvider = serverWorld.getChunkProvider();
+        BlockPos blockpos = chunkProvider.getStrongholdGen(serverWorld, structure, position);
+        if (blockpos == null) {
+            MapGenStructure gen = null;
+            final IChunkGenerator chunkGenerator = chunkProvider.chunkGenerator;
+            final String provider = chunkGenerator.getClass().getName();
+            try {
+                if (provider.equals("net.minecraft.world.gen.ChunkProviderOverworld")) {
+                    try {
+                        if (structure.equalsIgnoreCase("Village")) {
+                            gen = (MapGenStructure) FieldUtils.readField(chunkGenerator, "field_186005_x", true); // villageGenerator
+                        } else if (structure.equalsIgnoreCase("Monument")) {
+                            gen = (MapGenStructure) FieldUtils.readField(chunkGenerator, "field_185980_B", true); // oceanMonumentGenerator
+                        } else if (structure.equalsIgnoreCase("Temple")) {
+                            gen = (MapGenStructure) FieldUtils.readField(chunkGenerator, "field_186007_z", true); // MapGenScatteredFeature
+                        } else if (structure.equalsIgnoreCase("Mineshaft")) {
+                            gen = (MapGenStructure) FieldUtils.readField(chunkGenerator, "field_186006_y", true); // mineshaftGenerator
+                        }
+                    } catch (Exception e) {
+                        System.err.println(
+                                "Failed loading ChunkProviderOverworld field " + structure + ": " + e.toString());
+                    }
+                } else if (provider.equals("net.minecraft.world.gen.ChunkProviderHell")) {
+                    try {
+                        if (structure.equalsIgnoreCase("Fortress")) {
+                            gen = (MapGenStructure) FieldUtils.readField(chunkGenerator, "field_73172_c", true); // genNetherBridge
+                        }
+                    } catch (Exception e) {
+                        System.err.println(
+                                "Failed loading ChunkProviderHell field " + structure + ": " + e.toString());
+                    }
+                } else if (provider.equals("net.minecraft.world.gen.ChunkProviderEnd")) {
+                    try {
+                        if (structure.equalsIgnoreCase("EndCity")) {
+                            gen = (MapGenStructure) FieldUtils.readField(chunkGenerator, "field_185972_n", true); // endCityGen
+                        }
+                    } catch (Exception e) {
+                        System.err.println("Failed loading ChunkProviderEnd field " + structure + ": " + e.toString());
+                    }
+                } else if (provider.equals("com.khorn.terraincontrol.forge.generator.TXChunkGenerator")) {
+                    final Object world = FieldUtils.readField(chunkGenerator, "world", true);
+                    if (structure.equalsIgnoreCase("Village")) {
+                        gen = (MapGenStructure) FieldUtils.readField(world, "villageGen", true);
+                    } else if (structure.equalsIgnoreCase("Monument")) {
+                        gen = (MapGenStructure) FieldUtils.readField(world, "oceanMonumentGen", true);
+                    } else if (structure.equalsIgnoreCase("Temple")) {
+                        gen = (MapGenStructure) FieldUtils.readField(world, "rareBuildingGen", true);
+                    } else if (structure.equalsIgnoreCase("Mineshaft")) {
+                        gen = (MapGenStructure) FieldUtils.readField(world, "mineshaftGen", true);
+                    } else if (structure.equalsIgnoreCase("Fortress")) {
+                        gen = (MapGenStructure) FieldUtils.readField(world, "netherFortressGen", true);
+                    }
+                } else
+                    sender.sendMessage(new TextComponentString("Unknown Chunk provider " + provider));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            if (gen != null) {
+                try {
+                    blockpos = gen.getClosestStrongholdPos(serverWorld, position);
+                } catch (Exception e) {
+                    sender.sendMessage(new TextComponentString("No " + structure + " available"));
+                    return;
+                }
+            }
+        }
+        if (blockpos != null)
+            sender.sendMessage(new TextComponentString(structure + ": " + blockpos.getX() + ", " + blockpos.getZ()));
+        else
+            sender.sendMessage(new TextComponentString("Could not locate " + structure));
+    }
+
+    @Override
+    public List<String> getTabCompletions(MinecraftServer server, ICommandSender sender, String[] args, BlockPos pos) {
+        if (args.length == 1) {
+            return getListOfStringsMatchingLastWord(args, "Village", "Stronghold", "Monument", "Temple", "Mineshaft",
+                    "Fortress", "EndCity");
+        }
+        return Collections.emptyList();
+    }
+}
